5b06e219200fa74f8b53cc9dc2e866ec90b8f1df
Simon Dorrer <simon.dorrer@jku.at>
2025-01-05 20:19:13 +0100

    template is now working again

--- orfs/flow/results/ihp-sg13g2/counter_board/base/6_1_fill.v
+++ orfs/flow/results/ihp-sg13g2/counter_board/base/6_1_fill.v
@@ -7,80 +7,74 @@ module counter_board (clock_i,
  input reset_n_i;
  output [3:0] counter_value_o;
 
+ wire _00_;
+ wire _01_;
+ wire _02_;
+ wire _03_;
+ wire _04_;
+ wire _05_;
+ wire _06_;
+ wire _07_;
+ wire _08_;
+ wire _09_;
+ wire _10_;
  wire net3;
  wire net4;
  wire net5;
  wire net6;
- wire n1_o;
- wire \counter_0/_00_ ;
- wire \counter_0/_01_ ;
- wire \counter_0/_02_ ;
- wire \counter_0/_03_ ;
- wire \counter_0/_04_ ;
- wire \counter_0/_05_ ;
- wire \counter_0/_06_ ;
- wire \counter_0/_07_ ;
- wire \counter_0/_08_ ;
- wire \counter_0/_09_ ;
- wire \counter_0/_10_ ;
- wire \counter_0/_11_ ;
  wire net1;
  wire net2;
  wire clknet_0_clock_i;
  wire clknet_1_0__leaf_clock_i;
  wire clknet_1_1__leaf_clock_i;
 
- sg13g2_inv_2 _0_ (.Y(n1_o),
-    .A(net2));
- sg13g2_inv_2 \counter_0/_12_  (.Y(\counter_0/_00_ ),
-    .A(n1_o));
- sg13g2_xor2_1 \counter_0/_13_  (.B(net1),
+ sg13g2_xor2_1 _11_ (.B(net1),
     .A(net3),
-    .X(\counter_0/_01_ ));
- sg13g2_nand2_1 \counter_0/_14_  (.Y(\counter_0/_05_ ),
+    .X(_00_));
+ sg13g2_nand2_1 _12_ (.Y(_04_),
     .A(net3),
     .B(net1));
- sg13g2_xnor2_1 \counter_0/_15_  (.Y(\counter_0/_02_ ),
+ sg13g2_xnor2_1 _13_ (.Y(_01_),
     .A(net4),
-    .B(\counter_0/_05_ ));
- sg13g2_nand3_1 \counter_0/_16_  (.B(net3),
+    .B(_04_));
+ sg13g2_nand3_1 _14_ (.B(net3),
     .C(net1),
     .A(net4),
-    .Y(\counter_0/_06_ ));
- sg13g2_xnor2_1 \counter_0/_17_  (.Y(\counter_0/_03_ ),
+    .Y(_05_));
+ sg13g2_xnor2_1 _15_ (.Y(_02_),
     .A(net5),
-    .B(\counter_0/_06_ ));
- sg13g2_nand4_1 \counter_0/_18_  (.B(net3),
+    .B(_05_));
+ sg13g2_nand4_1 _16_ (.B(net3),
     .C(net5),
     .A(net4),
-    .Y(\counter_0/_07_ ),
+    .Y(_06_),
     .D(net1));
- sg13g2_xnor2_1 \counter_0/_19_  (.Y(\counter_0/_04_ ),
+ sg13g2_xnor2_1 _17_ (.Y(_03_),
     .A(net6),
-    .B(\counter_0/_07_ ));
- sg13g2_dfrbp_1 \counter_0/n20_q[0]$_DFFE_PP0P_  (.CLK(clknet_1_0__leaf_clock_i),
-    .RESET_B(\counter_0/_00_ ),
-    .D(\counter_0/_01_ ),
-    .Q_N(\counter_0/_11_ ),
+    .B(_06_));
+ sg13g2_dfrbp_1 \counter_0.n20_q[0]$_DFFE_PN0P_  (.CLK(clknet_1_0__leaf_clock_i),
+    .RESET_B(net2),
+    .D(_00_),
+    .Q_N(_10_),
     .Q(net3));
- sg13g2_dfrbp_1 \counter_0/n20_q[1]$_DFFE_PP0P_  (.CLK(clknet_1_0__leaf_clock_i),
-    .RESET_B(\counter_0/_00_ ),
-    .D(\counter_0/_02_ ),
-    .Q_N(\counter_0/_10_ ),
+ sg13g2_dfrbp_1 \counter_0.n20_q[1]$_DFFE_PN0P_  (.CLK(clknet_1_1__leaf_clock_i),
+    .RESET_B(net2),
+    .D(_01_),
+    .Q_N(_09_),
     .Q(net4));
- sg13g2_dfrbp_1 \counter_0/n20_q[2]$_DFFE_PP0P_  (.CLK(clknet_1_1__leaf_clock_i),
-    .RESET_B(\counter_0/_00_ ),
-    .D(\counter_0/_03_ ),
-    .Q_N(\counter_0/_09_ ),
+ sg13g2_dfrbp_1 \counter_0.n20_q[2]$_DFFE_PN0P_  (.CLK(clknet_1_0__leaf_clock_i),
+    .RESET_B(net2),
+    .D(_02_),
+    .Q_N(_08_),
     .Q(net5));
- sg13g2_dfrbp_1 \counter_0/n20_q[3]$_DFFE_PP0P_  (.CLK(clknet_1_1__leaf_clock_i),
-    .RESET_B(\counter_0/_00_ ),
-    .D(\counter_0/_04_ ),
-    .Q_N(\counter_0/_08_ ),
+ sg13g2_dfrbp_1 \counter_0.n20_q[3]$_DFFE_PN0P_  (.CLK(clknet_1_1__leaf_clock_i),
+    .RESET_B(net2),
+    .D(_03_),
+    .Q_N(_07_),
     .Q(net6));
  sg13g2_buf_1 input1 (.A(enable_i),
     .X(net1));
- sg13g2_buf_1 input2 (.A(reset_n_i),
+ sg13g2_buf_2 input2 (.A(reset_n_i),
     .X(net2));
  sg13g2_buf_1 output3 (.A(net3),
     .X(counter_value_o[0]));
@@ -157,112 +151,113 @@ module counter_board (clock_i,
  sg13g2_decap_8 FILLER_3_7 ();
  sg13g2_decap_8 FILLER_3_14 ();
  sg13g2_decap_8 FILLER_3_21 ();
- sg13g2_decap_8 FILLER_3_28 ();
- sg13g2_decap_8 FILLER_3_35 ();
- sg13g2_decap_8 FILLER_3_42 ();
- sg13g2_decap_8 FILLER_3_49 ();
- sg13g2_decap_8 FILLER_3_56 ();
- sg13g2_decap_8 FILLER_3_63 ();
- sg13g2_decap_8 FILLER_3_70 ();
- sg13g2_decap_8 FILLER_3_77 ();
- sg13g2_decap_8 FILLER_3_84 ();
- sg13g2_decap_8 FILLER_3_91 ();
- sg13g2_decap_8 FILLER_3_98 ();
- sg13g2_decap_8 FILLER_3_105 ();
- sg13g2_decap_8 FILLER_3_112 ();
- sg13g2_decap_4 FILLER_3_119 ();
- sg13g2_fill_1 FILLER_3_123 ();
- sg13g2_decap_8 FILLER_4_8 ();
- sg13g2_decap_8 FILLER_4_15 ();
- sg13g2_decap_8 FILLER_4_22 ();
- sg13g2_decap_8 FILLER_4_29 ();
- sg13g2_decap_8 FILLER_4_36 ();
- sg13g2_decap_8 FILLER_4_43 ();
- sg13g2_decap_8 FILLER_4_50 ();
- sg13g2_decap_8 FILLER_4_57 ();
- sg13g2_decap_8 FILLER_4_64 ();
- sg13g2_decap_8 FILLER_4_71 ();
- sg13g2_decap_8 FILLER_4_78 ();
- sg13g2_decap_8 FILLER_4_85 ();
- sg13g2_decap_8 FILLER_4_92 ();
- sg13g2_decap_8 FILLER_4_99 ();
- sg13g2_decap_8 FILLER_4_106 ();
- sg13g2_decap_8 FILLER_4_113 ();
- sg13g2_decap_4 FILLER_4_120 ();
- sg13g2_fill_2 FILLER_5_0 ();
- sg13g2_decap_8 FILLER_5_28 ();
- sg13g2_decap_8 FILLER_5_35 ();
- sg13g2_decap_8 FILLER_5_42 ();
- sg13g2_decap_8 FILLER_5_49 ();
- sg13g2_decap_8 FILLER_5_56 ();
- sg13g2_decap_8 FILLER_5_63 ();
- sg13g2_decap_8 FILLER_5_70 ();
- sg13g2_decap_8 FILLER_5_77 ();
- sg13g2_decap_8 FILLER_5_84 ();
- sg13g2_decap_8 FILLER_5_91 ();
- sg13g2_decap_8 FILLER_5_98 ();
- sg13g2_decap_8 FILLER_5_105 ();
- sg13g2_decap_8 FILLER_5_112 ();
- sg13g2_decap_4 FILLER_5_119 ();
- sg13g2_fill_1 FILLER_5_123 ();
- sg13g2_fill_1 FILLER_6_9 ();
- sg13g2_fill_1 FILLER_6_14 ();
- sg13g2_decap_8 FILLER_6_49 ();
- sg13g2_decap_8 FILLER_6_56 ();
- sg13g2_decap_8 FILLER_6_63 ();
- sg13g2_decap_8 FILLER_6_70 ();
- sg13g2_decap_8 FILLER_6_77 ();
- sg13g2_decap_8 FILLER_6_84 ();
- sg13g2_decap_8 FILLER_6_91 ();
- sg13g2_decap_8 FILLER_6_98 ();
- sg13g2_decap_8 FILLER_6_105 ();
- sg13g2_decap_8 FILLER_6_112 ();
- sg13g2_decap_4 FILLER_6_119 ();
- sg13g2_fill_1 FILLER_6_123 ();
+ sg13g2_fill_2 FILLER_3_28 ();
+ sg13g2_decap_8 FILLER_3_34 ();
+ sg13g2_decap_8 FILLER_3_41 ();
+ sg13g2_decap_8 FILLER_3_48 ();
+ sg13g2_decap_8 FILLER_3_55 ();
+ sg13g2_decap_8 FILLER_3_62 ();
+ sg13g2_decap_8 FILLER_3_69 ();
+ sg13g2_decap_8 FILLER_3_76 ();
+ sg13g2_decap_8 FILLER_3_83 ();
+ sg13g2_decap_8 FILLER_3_90 ();
+ sg13g2_decap_8 FILLER_3_97 ();
+ sg13g2_decap_8 FILLER_3_104 ();
+ sg13g2_decap_8 FILLER_3_111 ();
+ sg13g2_decap_4 FILLER_3_118 ();
+ sg13g2_fill_2 FILLER_3_122 ();
+ sg13g2_decap_8 FILLER_4_4 ();
+ sg13g2_decap_8 FILLER_4_11 ();
+ sg13g2_decap_8 FILLER_4_18 ();
+ sg13g2_decap_8 FILLER_4_25 ();
+ sg13g2_decap_8 FILLER_4_32 ();
+ sg13g2_decap_8 FILLER_4_39 ();
+ sg13g2_decap_8 FILLER_4_46 ();
+ sg13g2_decap_8 FILLER_4_53 ();
+ sg13g2_decap_8 FILLER_4_60 ();
+ sg13g2_decap_8 FILLER_4_67 ();
+ sg13g2_decap_8 FILLER_4_74 ();
+ sg13g2_decap_8 FILLER_4_81 ();
+ sg13g2_decap_8 FILLER_4_88 ();
+ sg13g2_decap_8 FILLER_4_95 ();
+ sg13g2_decap_8 FILLER_4_102 ();
+ sg13g2_decap_8 FILLER_4_109 ();
+ sg13g2_decap_8 FILLER_4_116 ();
+ sg13g2_fill_1 FILLER_4_123 ();
+ sg13g2_fill_1 FILLER_5_0 ();
+ sg13g2_decap_8 FILLER_5_27 ();
+ sg13g2_decap_8 FILLER_5_34 ();
+ sg13g2_decap_8 FILLER_5_41 ();
+ sg13g2_decap_8 FILLER_5_48 ();
+ sg13g2_decap_8 FILLER_5_55 ();
+ sg13g2_decap_8 FILLER_5_62 ();
+ sg13g2_decap_8 FILLER_5_69 ();
+ sg13g2_decap_8 FILLER_5_76 ();
+ sg13g2_decap_8 FILLER_5_83 ();
+ sg13g2_decap_8 FILLER_5_90 ();
+ sg13g2_decap_8 FILLER_5_97 ();
+ sg13g2_decap_8 FILLER_5_104 ();
+ sg13g2_decap_8 FILLER_5_111 ();
+ sg13g2_decap_4 FILLER_5_118 ();
+ sg13g2_fill_2 FILLER_5_122 ();
+ sg13g2_decap_8 FILLER_6_47 ();
+ sg13g2_decap_8 FILLER_6_54 ();
+ sg13g2_decap_8 FILLER_6_61 ();
+ sg13g2_decap_8 FILLER_6_68 ();
+ sg13g2_decap_8 FILLER_6_75 ();
+ sg13g2_decap_8 FILLER_6_82 ();
+ sg13g2_decap_8 FILLER_6_89 ();
+ sg13g2_decap_8 FILLER_6_96 ();
+ sg13g2_decap_8 FILLER_6_103 ();
+ sg13g2_decap_8 FILLER_6_110 ();
+ sg13g2_decap_8 FILLER_6_117 ();
  sg13g2_fill_1 FILLER_7_0 ();
- sg13g2_decap_8 FILLER_7_35 ();
- sg13g2_decap_8 FILLER_7_42 ();
- sg13g2_decap_8 FILLER_7_49 ();
- sg13g2_decap_8 FILLER_7_56 ();
- sg13g2_decap_8 FILLER_7_63 ();
- sg13g2_decap_8 FILLER_7_70 ();
- sg13g2_decap_8 FILLER_7_77 ();
- sg13g2_decap_8 FILLER_7_84 ();
- sg13g2_fill_2 FILLER_7_91 ();
- sg13g2_fill_1 FILLER_7_93 ();
- sg13g2_decap_8 FILLER_7_102 ();
- sg13g2_decap_8 FILLER_7_109 ();
- sg13g2_decap_8 FILLER_7_116 ();
+ sg13g2_fill_1 FILLER_7_5 ();
+ sg13g2_fill_2 FILLER_7_12 ();
+ sg13g2_decap_8 FILLER_7_48 ();
+ sg13g2_decap_8 FILLER_7_55 ();
+ sg13g2_decap_8 FILLER_7_62 ();
+ sg13g2_decap_8 FILLER_7_69 ();
+ sg13g2_decap_8 FILLER_7_76 ();
+ sg13g2_decap_8 FILLER_7_83 ();
+ sg13g2_decap_8 FILLER_7_90 ();
+ sg13g2_decap_8 FILLER_7_97 ();
+ sg13g2_fill_2 FILLER_7_104 ();
+ sg13g2_fill_1 FILLER_7_106 ();
+ sg13g2_fill_2 FILLER_7_111 ();
+ sg13g2_fill_1 FILLER_7_113 ();
  sg13g2_fill_1 FILLER_7_123 ();
  sg13g2_fill_2 FILLER_8_0 ();
- sg13g2_decap_8 FILLER_8_58 ();
- sg13g2_decap_8 FILLER_8_65 ();
- sg13g2_decap_8 FILLER_8_72 ();
- sg13g2_decap_8 FILLER_8_79 ();
- sg13g2_decap_8 FILLER_8_86 ();
- sg13g2_decap_8 FILLER_8_93 ();
- sg13g2_decap_8 FILLER_8_100 ();
- sg13g2_decap_8 FILLER_8_107 ();
- sg13g2_decap_8 FILLER_8_114 ();
- sg13g2_fill_2 FILLER_8_121 ();
- sg13g2_fill_1 FILLER_8_123 ();
- sg13g2_fill_1 FILLER_9_12 ();
- sg13g2_decap_8 FILLER_9_17 ();
- sg13g2_decap_8 FILLER_9_24 ();
- sg13g2_decap_8 FILLER_9_31 ();
- sg13g2_decap_8 FILLER_9_38 ();
- sg13g2_decap_8 FILLER_9_45 ();
- sg13g2_decap_8 FILLER_9_52 ();
- sg13g2_decap_8 FILLER_9_59 ();
- sg13g2_decap_8 FILLER_9_66 ();
- sg13g2_decap_8 FILLER_9_73 ();
- sg13g2_decap_8 FILLER_9_80 ();
- sg13g2_decap_8 FILLER_9_87 ();
- sg13g2_decap_8 FILLER_9_94 ();
- sg13g2_decap_8 FILLER_9_101 ();
- sg13g2_decap_8 FILLER_9_108 ();
- sg13g2_decap_8 FILLER_9_115 ();
- sg13g2_fill_2 FILLER_9_122 ();
+ sg13g2_fill_2 FILLER_8_10 ();
+ sg13g2_decap_8 FILLER_8_38 ();
+ sg13g2_decap_8 FILLER_8_45 ();
+ sg13g2_decap_8 FILLER_8_52 ();
+ sg13g2_decap_8 FILLER_8_59 ();
+ sg13g2_decap_8 FILLER_8_66 ();
+ sg13g2_decap_8 FILLER_8_73 ();
+ sg13g2_decap_8 FILLER_8_80 ();
+ sg13g2_decap_8 FILLER_8_87 ();
+ sg13g2_decap_8 FILLER_8_94 ();
+ sg13g2_decap_8 FILLER_8_101 ();
+ sg13g2_decap_8 FILLER_8_108 ();
+ sg13g2_decap_8 FILLER_8_115 ();
+ sg13g2_fill_2 FILLER_8_122 ();
+ sg13g2_decap_8 FILLER_9_8 ();
+ sg13g2_decap_8 FILLER_9_15 ();
+ sg13g2_decap_8 FILLER_9_26 ();
+ sg13g2_decap_8 FILLER_9_33 ();
+ sg13g2_decap_8 FILLER_9_40 ();
+ sg13g2_decap_8 FILLER_9_47 ();
+ sg13g2_decap_8 FILLER_9_54 ();
+ sg13g2_decap_8 FILLER_9_61 ();
+ sg13g2_decap_8 FILLER_9_68 ();
+ sg13g2_decap_8 FILLER_9_75 ();
+ sg13g2_decap_8 FILLER_9_82 ();
+ sg13g2_decap_8 FILLER_9_89 ();
+ sg13g2_decap_8 FILLER_9_96 ();
+ sg13g2_decap_8 FILLER_9_103 ();
+ sg13g2_decap_8 FILLER_9_110 ();
+ sg13g2_decap_8 FILLER_9_117 ();
  sg13g2_decap_8 FILLER_10_0 ();
  sg13g2_decap_8 FILLER_10_7 ();
  sg13g2_decap_8 FILLER_10_14 ();
@@ -304,22 +299,21 @@ module counter_board (clock_i,
  sg13g2_decap_8 FILLER_12_0 ();
  sg13g2_decap_8 FILLER_12_7 ();
  sg13g2_decap_8 FILLER_12_14 ();
- sg13g2_decap_8 FILLER_12_21 ();
- sg13g2_decap_8 FILLER_12_28 ();
- sg13g2_decap_8 FILLER_12_35 ();
- sg13g2_decap_8 FILLER_12_42 ();
- sg13g2_decap_8 FILLER_12_49 ();
- sg13g2_decap_8 FILLER_12_56 ();
- sg13g2_decap_8 FILLER_12_63 ();
- sg13g2_decap_8 FILLER_12_70 ();
- sg13g2_decap_8 FILLER_12_77 ();
- sg13g2_decap_8 FILLER_12_84 ();
- sg13g2_decap_8 FILLER_12_91 ();
- sg13g2_decap_8 FILLER_12_98 ();
- sg13g2_decap_8 FILLER_12_105 ();
- sg13g2_decap_8 FILLER_12_112 ();
- sg13g2_decap_4 FILLER_12_119 ();
- sg13g2_fill_1 FILLER_12_123 ();
+ sg13g2_fill_1 FILLER_12_21 ();
+ sg13g2_decap_8 FILLER_12_26 ();
+ sg13g2_decap_8 FILLER_12_33 ();
+ sg13g2_decap_8 FILLER_12_40 ();
+ sg13g2_decap_8 FILLER_12_47 ();
+ sg13g2_decap_8 FILLER_12_54 ();
+ sg13g2_decap_8 FILLER_12_61 ();
+ sg13g2_decap_8 FILLER_12_68 ();
+ sg13g2_decap_8 FILLER_12_75 ();
+ sg13g2_decap_8 FILLER_12_82 ();
+ sg13g2_decap_8 FILLER_12_89 ();
+ sg13g2_decap_8 FILLER_12_96 ();
+ sg13g2_decap_8 FILLER_12_103 ();
+ sg13g2_decap_8 FILLER_12_110 ();
+ sg13g2_decap_8 FILLER_12_117 ();
  sg13g2_decap_8 FILLER_13_0 ();
  sg13g2_decap_8 FILLER_13_7 ();
  sg13g2_decap_8 FILLER_13_14 ();
--- orfs/flow/results/ihp-sg13g2/counter_board/base/updated_clks.sdc
+++ orfs/flow/results/ihp-sg13g2/counter_board/base/updated_clks.sdc
@@ -5,7 +5,7 @@ current_design counter_board
 ###############################################################################
 # Timing Constraints
 ###############################################################################
-create_clock -name core_clock -period 24.2351 [get_ports {clock_i}]
+create_clock -name core_clock -period 24.2410 [get_ports {clock_i}]
 set_input_delay 25.0000 -clock [get_clocks {core_clock}] -add_delay [get_ports {enable_i}]
 set_input_delay 25.0000 -clock [get_clocks {core_clock}] -add_delay [get_ports {reset_n_i}]
 set_output_delay 25.0000 -clock [get_clocks {core_clock}] -add_delay [get_ports {counter_value_o[0]}]
