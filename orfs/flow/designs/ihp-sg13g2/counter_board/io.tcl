420404a8a418d61c89dfce43277e24a075f238d8
Simon Dorrer <simon.dorrer@jku.at>
2025-07-28 17:21:44 +0200

    fixed io.tcl warnings

--- orfs/flow/designs/ihp-sg13g2/counter_board/io.tcl
+++ orfs/flow/designs/ihp-sg13g2/counter_board/io.tcl
@@ -1,2 +1,3 @@
-set_io_pin_constraint -pin_names {clock_i, enable_i, reset_n_i} -region left:*
+set_io_pin_constraint -pin_names {clock_i enable_i reset_n_i} -region left:*
+# set_io_pin_constraint -pin_names {reg0[*]} -region left:* # Example for Bus
 set_io_pin_constraint -pin_names {counter_value_o} -region right:*
